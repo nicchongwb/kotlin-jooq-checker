b5abe9cc692d1627a930d479654ac1fa8e8c3334
nicchongwb <nicholaschongwb@gmail.com>
2024-08-16 01:12:39 +0800

    feat: add PlainSQLCheckerUtils

--- kotlin-ir-plugin/src/main/kotlin/com/nicchongwb/plugins/PlainSQLCheckerUtils.kt
+++ kotlin-ir-plugin/src/main/kotlin/com/nicchongwb/plugins/PlainSQLCheckerUtils.kt
@@ -1,5 +1,25 @@
 package com.nicchongwb.plugins
 
-// Helper 
-class PlainSQLChecker {
+import org.jetbrains.kotlin.ir.IrElement
+import org.jetbrains.kotlin.ir.declarations.IrClass
+import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
+import org.jetbrains.kotlin.ir.util.render
+import org.jooq.Allow
+fun getAllowPlainSQLAnnotation(element: IrElement): String {
+  val annotations = when (element) {
+    is IrClass -> element.annotations
+    is IrSimpleFunction -> element.annotations
+    else -> return ""
+  }
+
+  return annotations.find {
+    it.type.render() == Allow.PlainSQL::class.qualifiedName
+  }?.type?.render()?.toString() ?: ""
+}
+
+fun isAllowPlainSQLAnnotation(data: String): Boolean {
+  if (data == Allow.PlainSQL::class.qualifiedName) {
+    return true
+  }
+  return false
 }
