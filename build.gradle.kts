4f30e42e7191731cf79cb747af93487172e8c8d3
nicchongwb <nicholaschongwb@gmail.com>
2024-08-17 22:54:24 +0800

    chore: fix build gradle & kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ buildscript {
 }
 
 plugins {
-  kotlin("jvm") version "1.9.22" apply false
-  id("org.jetbrains.dokka") version "1.9.10" apply false
+  kotlin("jvm") version "1.9.0" apply false
+  id("org.jetbrains.dokka") version "1.9.0" apply false
   id("com.gradle.plugin-publish") version "1.2.1" apply false
   id("com.github.gmazzo.buildconfig") version "5.3.5" apply false
   id("nu.studer.jooq") version "8.0" apply false
