e9fd1eec710041bc3b2b8d04c791bc4a959d261c
nicchongwb <nicholaschongwb@gmail.com>
2024-08-17 12:28:32 +0800

    chore: add test case

---
+++ kotlin-ir-plugin/src/test/kotlin/com/nicchongwb/plugins/TestTools.kt
@@ -0,0 +1,31 @@
+@file:OptIn(ExperimentalCompilerApi::class)
+
+package com.nicchongwb.plugins
+
+import com.tschuchort.compiletesting.JvmCompilationResult
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
+import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
+
+// Plugin configuration
+val debugDumpIR = true
+
+fun compile(
+  sourceFiles: List<SourceFile>,
+  plugin: CompilerPluginRegistrar = KotlinJooqCheckerCompilerRegistrar(defaultDebugDumpIR = debugDumpIR),
+): JvmCompilationResult {
+  return KotlinCompilation().apply {
+    sources = sourceFiles
+    compilerPluginRegistrars = listOf(plugin)
+    inheritClassPath = true
+//    messageOutputStream = System.out
+  }.compile()
+}
+
+fun compile(
+  sourceFile: SourceFile,
+  plugin: CompilerPluginRegistrar = KotlinJooqCheckerCompilerRegistrar(defaultDebugDumpIR = debugDumpIR),
+): JvmCompilationResult {
+  return compile(listOf(sourceFile), plugin)
+}
---
+++ kotlin-ir-plugin/src/test/kotlin/com/nicchongwb/plugins/sources/PlainSqlSourceFile.kt
@@ -0,0 +1,33 @@
+package com.nicchongwb.plugins.sources
+
+import org.jooq.Record
+import org.jooq.Result
+import org.jooq.SelectConditionStep
+import org.jooq.impl.DSL
+import java.sql.DriverManager
+
+class PlainSqlSourceFile {
+  val url = "jdbc:h2:~/test;AUTO_SERVER=TRUE"
+  val username = "sa"
+  val password = ""
+
+  private val dsl = DriverManager.getConnection(url, username, password).use {
+    DSL.using(it)
+  }
+
+  fun findAll(): SelectConditionStep<Record> {
+    return dsl.select()
+      .from(DSL.table("test_table"))
+      .where(DSL.field("test_column").eq("test_value"))
+      .and("test_condition_a")
+      .and(DSL.field("test_condition_b").eq("condition_c"))
+  }
+
+  fun plainSqlFindByStringId(id: String): Result<Record> {
+    return dsl.fetch("SELECT * FROM test_table WHERE id=" + id)
+  }
+
+  fun plainSqlUpdateQuery(username: String, oldPass: String, newPass: String): Result<Record> {
+    return dsl.fetch("UPDATE test_table SET pass='" + newPass + "' WHERE username='" + username + "' AND password='" + oldPass + "'")
+  }
+}
